816dc8ba16960c297dbd01598d826876fc02664f
m-faizan-adill <adilfaizan423@gmail.com>
2026-05-16 13:53:28 +0500

    utilize logger in db, errorHandler and controllers

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,13 @@
 import mongoose from 'mongoose'
 import { MONGO_URI } from './env.js'
+import logger from './logger.js';
 
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGO_URI)
-    console.log('MongoDB Connected')
+    logger.info('MongoDB Connected')
   } catch (err) {
-    console.log(err)
+    logger.error(`MongoDB Connection Failed: ${err.message}`)
     process.exit(1)
   }
 }
--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,10 @@
+import logger from "../config/logger.js"
+
 const errorHandler = (err, req, res, next) => {
     const statusCode = err.statusCode || 500
     const message = err.message || 'Internal Server Error'
+
+    logger.error(`${statusCode} - ${message}`)
 
     res.status(statusCode).json({
         success: false,
