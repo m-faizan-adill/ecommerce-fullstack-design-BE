86f2873747dfb2983c532568ed8276bf8943b05d
m-faizan-adill <adilfaizan423@gmail.com>
2026-05-16 14:05:53 +0500

    change port

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-export const PORT = process.env.PORT || 3000
+export const PORT = process.env.PORT || 5000
 export const MONGO_URI = process.env.MONGO_URI
