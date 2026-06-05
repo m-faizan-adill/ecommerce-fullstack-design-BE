3394fcbea70f1375bc8a4792674d6ca5fce4a877
m-faizan-adill <adilfaizan423@gmail.com>
2026-06-05 21:06:51 +0500

    feat: add not found middleware for unmatched routes

--- src/server.js
+++ src/server.js
@@ -1,12 +1,15 @@
 import express from 'express'
 import errorHandler from './middleware/errorHandler.js'
 import routes from './routes/index.js'
+import notFoundHandler from './middleware/notFoundHandler.js'
 
 const app = express()
 
 app.use(express.json())
 
 app.use('/api/v1', routes);
+
+app.use(notFoundHandler);
 
 app.use(errorHandler)
 
